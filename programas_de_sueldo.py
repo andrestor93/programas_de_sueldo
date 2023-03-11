32f78f4df8b4e4fb694bd9341d113d01b99427e0
andrestor93 <underthecurve@outlook.com>
2023-03-10 19:01:29 -0500

    programa de nomina basico

---
+++ programas_de_sueldo.py
@@ -0,0 +1,7 @@
+horas = int(input("ingrese las horas laboradas: "))
+valor = int(input("ingrese el valor de las horas laboradas: "))
+
+sueldo = horas*valor
+
+print("el sueldo del trabajador es: ", sueldo)
+
